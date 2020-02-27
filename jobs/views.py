c0ce525e2a2a1a1b8b6b0310df3de420bb32daf6
nanthu0123 <nanthu0123@gmail.com>
2020-02-27 03:20:17 -0800

    bootstrap and job page
    bootstrap added and html codes are modified,then jobs web page showing up.

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
 # Create your views here.
+from . models import job
+
+
+
+def home(request):
+    jobs=job.objects
+    return render(request,'jobs/home.html',{'jobs':jobs})
